bd45912f13df7e4fa98bdd2b359e21fe8e012489
Aswathy V <141418535+aswathy-vijayan@users.noreply.github.com>
2024-01-08 19:14:35 +0530

    Update AutomationConstants.java

--- Group4Project/ICTAKWebsite/src/main/java/ictwebsite/constants/AutomationConstants.java
+++ Group4Project/ICTAKWebsite/src/main/java/ictwebsite/constants/AutomationConstants.java
@@ -2,9 +2,31 @@ package ictwebsite.constants;
 
 public class AutomationConstants 
 {
+	public static  String name = "ICT Academy of Kerala";
+	public static  String CO_NAME = "Our Courses";
+	public static  String test = "ABOUT US";
+	public static String soft ="SoftwareTesting";
+	public static String username1 = "adam";
+	public static String mobilenum1 = " 9987456778";
+	public static String email1 = "ad.123@gmail.com";
 
-	 public static  String name = "ICT Academy of Kerala";
-	 public static  String CO_NAME = "Our Courses";
-	 public static  String test = "ABOUT US";
-	 public static String soft ="SoftwareTesting";
+	public static String mobilenum2 = "adfhjkl";
+	public static String email2= "ad.123@g.com";
+
+	public static String username3 = " ";
+	public static String mobilenum3 = " ";
+	public static String email3 = " ";
+
+	public static String username4 = "348766";
+	public static String mobilenum4 = "9987456778";
+	public static String email4 = "ad.123@gmail.com";
+
+	public static String ICSETloading = "http://64.227.132.109/LandingPage/event/61d88c6022c79c40406009e7";
+	public static String agendaExpected = "Agenda";
+	public static String regSuccessMsg = "Registration Successfull";
+	public static String regUnsuccess = "Registration Unsuccessful due to wrong credentials";
+	public static String getAcess = "Join us today";
+	public static String trainingval = "Training";
+	public static String techURL= "http://64.227.132.109/LandingPage/event/61d88e9322c79c40406009eb";
+	public static String abt = " Techathlon ";
 }
